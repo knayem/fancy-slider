8afe18308df6c756695fe0eb5cec353b9ce940a9
knayem <abs99knayem@gmail.com>
2021-02-14 23:57:44 +0600

    first commit

--- app.js
+++ app.js
@@ -55,17 +55,20 @@
     let slideIndex = 0;
     const selectItem = (event, img) => {
       let element = event.target;
-      element.classList.add('added');
+      element.classList.toggle('added');
 
       let item = sliders.indexOf(img);
       if (item === -1) {
         sliders.push(img);
    
 
-  } else {
+  } 
         
+    else {
+        sliders.splice(item);
         element.classList.toggle('added');}
 }
+
     var timer
     const createSlider = () => {
       // check slider image length
